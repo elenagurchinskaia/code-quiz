19f5a1271e488b803699f75834420d11948522be
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-06 20:19:27 -0500

    updated java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-// pseudocode created in a group session
 var startBtn = document.querySelector("#start-quiz-button");
 var questionEl = document.querySelector("#question");
+var answerEl = document.querySelector("#answer-choices");
 var startEl = document.querySelector("#start-container");
 var quizEl = document.querySelector("#quiz-container");
-// Global variables
+var timerEl = document.querySelector("#timer");
+var answersArr = document.querySelectorAll(".answer-button");
+// globar variables
+var timerInterval;
 var score = 0;
-var timeLeft = 200;
+var timeLeft = 60;
 var questions = [
   "What is the proper term for group of kittens?",
   "All cats are born with what color eyes?",
@@ -15,6 +18,8 @@ var questions = [
   "What is the normal body temperature of a cat?",
   "What breed of a cat has a reputation for being cross-eyed?",
 ];
+
+// var made of arrays of strings
 var answersChoices = [
   ["KABOODLE", "KINDLE", "KINE", "KETTLE"],
   ["BLUE", "PINK", "GREEN", "BLACK"],
@@ -23,54 +28,136 @@ var answersChoices = [
   ["210", "140", "280", "70"],
   ["102 FAHRENHEIT", "98 FAHRENHEIT", "94 FAHRENHEIT", "106 FAHRENHEIT"],
   ["EGYPTIAN MAU", "SIAMESE", "HIMALAYAN", "PERSIAN"],
-]; // made of arrays of strings
-var correctAnswer = ["1", "0", "2", "1", "3", "0", "1"]; // made of strings
+];
+var correctAnswer = ["1", "0", "2", "1", "3", "0", "1"];
 var currentQuestion = 0;
-// All of our selectors (document.)
 
-// function
-function start() {
-  // triggered when they pressed button (event listener)
-  // starts the timer
-  // > set interval
-  // > once timer hits 0, call endQuiz function
-  // hide the start button
-  // reveal the options, questions
+// All of our selectors (document.)
+// triggered when they pressed button (event listener)
+// starts the timer
+// > set interval
+// > once timer hits 0, call endQuiz function
+// hide the start button
+// reveal the options, questions
+
+function startTimer() {
+  console.log("quiz-started");
+  timeLeft = 60;
+  // var timerDisplay = document.getElementById("timer");
+  timerEl.textContent = "Time: " + timeLeft;
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+  }, 1000);
 }
 
+// changeAnswersChoices();
+// triggered when the user selects any answer
+// guess what answer the user choose
+// guess if the answer is right or wrong
+// > if it's wrong, reduce the time by 10, then show message "Wrong!"
+// > if it's right, then show message "Correct!" (time left at the end is the user's score)
+// change the question
+// change the choices
+
 function nextQuestion(event) {
-  // triggered when the user selects any answer
-  // guess what answer the user choose
-  // guess if the answer is right or wrong
-  // > if it's wrong, reduce the time, then show message "Wrong!"
-  // > if it's right, then show message "Correct!" (time left at the end is the user's score)
-  // increment the currect question by 1
-  // change the question
-  // change the choices
+  var selectedAnswer = event.target.textContent;
+  if (currentQuestion > questionEl.length) {
+    endQuiz();
+    return;
+  }
+  // var isCorrect = checkAnswer(selectedAnswer);
+  // if (isCorrect) {
+  //   displayMessage("Correct!");
+  // } else {
+  //   displayMessage("Wrong!");
+  //   reduceTime(10);
+  // }
+  changeQuestions();
+}
+
+function checkAnswer(answer) {
+  // if answer is = to correrct answer , then return true
+  // if not, return false
+  if (answer === correctAnswer) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// increment the currect question by 1
+function changeQuestions() {
+  if (questions[currentQuestion]) {
+    questionEl.textContent = questions[currentQuestion];
+
+    for (var i = 0; i < answersArr.length; i++) {
+      answersArr[i].textContent = answersChoices[currentQuestion][i];
+    }
+    currentQuestion++;
+  } else {
+    endQuiz();
+  }
 }
 
 function endQuiz() {
   // triggered either when timeLeft becomes 0 or when the user finishes all questions
+  // var initials = prompt("Please enter your initials:");
   // prompts the user for initials, displays the score
+  // var score = timeLeft;
+  // alert("Your score is:" + score);
   // hide the question
+  // quizEl = document.getElementById("#quiz-container");
+  quizEl.innerHTML = " ";
+  clearInterval(timerInterval);
+  submitInitials();
   // if the user finishes before timer runs out, stop the timer from running
 }
 
+// save scores and initials to the local storage
+// > save the existing score to the variable
+// > add the new scores to the end of the new array
+// > overwrite the scores with the new array
+// take the user to the highScore.html page
+// triggered when the user submitted their initials
 function submitInitials() {
   submitInitials.addEventListener("");
-  // triggered when the user submitted their initials
-  // save scores and initials to the local storage
-  // > save the existing score to the variable
-  // > add the new scores to the end of the new array
-  // > overwrite the scores with the new array
-  // take the user to the highScore.html page
+  var initialsInput = document.getElementById("initials-input");
+  var initials = initialsInput.value;
+  var score = JSON.parse(localStorage.getItem("scores")) || [];
+  score.push({ initials: initials, score: score });
+  localStorage.setItem("scores", JSON.stringlify(scores));
+  window.location.href = "highScore.html";
+  var submitBtn = document.getElementById("submit-button");
+  submitButton.addEventListener("click", submitInitials);
 }
 
 // event listener to trigger start function
-
+// startBtn.addEventListener("click", startTimer);
 startBtn.addEventListener("click", function () {
   startEl.style.display = "none";
   quizEl.style.display = "block";
-  questionEl.textContent = questions[0];
-  answersChoices.textContent = answersChoices[0];
+  changeQuestions();
+  startTimer();
+});
+
+// for loop to add event listener to an array of answer elements
+for (var i = 0; i < answersArr.length; i++) {
+  answersArr[i].addEventListener("click", nextQuestion);
+}
+
+answersArr.forEach(function (answersChoices) {
+  answersChoices.addEventListener("click", function (event) {
+    var selectedAnswer = event.target.textContent;
+    var currentQuestion = questionEl;
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+    } else {
+      timeLeft -= 10;
+    }
+    changeQuestions();
+  });
 });
